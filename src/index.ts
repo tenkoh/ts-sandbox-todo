a3d03fb55abd528d7ef5961235b187214a7993a0
tenkoh <tenkoh.go@gmail.com>
2025-01-26 15:02:38 +0900

    ユーザー入力機能を追加

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import { createInterface } from "node:readline";
+const rl = createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
 class TodoItem {
 	title: string;
 	body?: string;
@@ -8,8 +14,8 @@ class TodoItem {
 	constructor(
 		title: string,
 		createdAt: Date,
-		body = undefined,
-		deadline = undefined,
+		body: string | undefined = undefined,
+		deadline: Date | undefined = undefined,
 	) {
 		this.title = title;
 		this.createdAt = createdAt;
@@ -39,8 +45,38 @@ class TodoStore {
 			return todo[key] === value;
 		});
 	}
+
+	append(title: string, body: string | undefined = undefined) {
+		this.todos.push(new TodoItem(title, new Date(), body, undefined));
+	}
 }
 
 const store = new TodoStore(todos);
 const filtered = store.filterBy("status", "progress");
 console.log(filtered);
+
+function askQuestion(prompt: string, must = false): Promise<string> {
+	return new Promise((resolve) => {
+		const askAgain = () => {
+			rl.question(prompt, (answer) => {
+				if (!must || (must && answer.trim() !== "")) {
+					resolve(answer);
+				} else {
+					console.log("input required");
+					askAgain();
+				}
+			});
+		};
+		askAgain();
+	});
+}
+
+async function addTodo(store: TodoStore) {
+	const title = await askQuestion("TODOのタイトルを追加してください", true);
+	const body = await askQuestion("(任意)詳細を入力してください");
+	store.append(title, body === "" ? undefined : body);
+}
+
+await addTodo(store);
+console.log(store.todos);
+rl.close();
